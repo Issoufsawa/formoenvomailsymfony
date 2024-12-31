3bb96ac1b9da0e81eb21d6714a43bbc2cb0d2a6c
Issoufsawa <sawadogoissoufoo7@gmail.com>
2024-12-31 10:44:40 +0100

    third commit

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'app_contact')]
+    public function index(Request $request): Response
+    {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid() ){
+            $data = $form->getData();
+            dd($data);
+        }
+
+        return $this->render('contact/index.html.twig', [
+            'controller_name' => 'ContactController',
+            'formulaire' => $form
+        ]);
+    }
+}
